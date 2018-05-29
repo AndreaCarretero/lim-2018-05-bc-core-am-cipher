9a276df3869929f00f84fb8fbaf6f648edba3ff8
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-05-29 13:03:28 -0500

    Ya tengo las funcionesgit status

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,49 @@
 window.cipher = {
   // ... creando objeto- y función
-  encode:(text,offset) =>{
-    let array=text.split("");
-    let textcod= '';
+  encode: (offset, string) => {
+    let numberAscii;
     let newnumberAscii;
-    for(i=0;i<text.length;i++){
-      console.log(array);
-    let  numberAscii=array[i].charCodeAt(0); // asigno una variable donde obtenga una caja que se
-    // almacene un texto y extraiga la posición ascii de cada letra que me da el arreglo(conjunto de espacios)
-    if(numberAscii>=65 && 90>=numberAscii){      
-    newnumberAscii= (numberAscii-65+offset)%26 +65;
-    let letterAscii=String.fromCharCode(newnumberAscii);
-    textcod= textcod.concat(letterAscii);
-    }
-    else{
-      if(numberAscii>=97 && 122>=numberAscii){
-        newnumberAscii= (numberAscii-97+offset)%26 +97;
-        let letterAscii=String.fromCharCode(newnumberAscii);
-        textcod= textcod.concat(letterAscii);
+    let letterAscii;
+    let textcod = '';
+    for (i = 0; i < string.length; i++) {
+      numberAscii = string.charCodeAt(i); // asigno una variable donde obtenga una caja que se
+      // almacene un texto y extraiga la posición ascii de cada letra que me da el arreglo(conjunto de espacios)
+      if (numberAscii >= 65 && numberAscii <= 90) {
+        newnumberAscii = (numberAscii - 65 + offset) % 26 + 65;
+        letterAscii = String.fromCharCode(newnumberAscii);
+      } else {
+        if (numberAscii >= 97 && numberAscii <= 122) {
+          newnumberAscii = (numberAscii - 97 + offset) % 26 + 97;
+          letterAscii = String.fromCharCode(newnumberAscii);
+        } else {
+          letterAscii = string.charAt(i);
+        }
       }
+      textcod = textcod.concat(letterAscii);
     }
-}
-return document.getElementById('result').innerHTML=textcod;
-}
-}
-// // creando función y colocand fórmula para cifrar texto
-  // // fórmula 
+    return textcod;
+  },
+  decode: (offset, string) => {
+    let numberAscii;
+    let newnumberAscii;
+    let letterAscii;
+    let textcod = '';
+    for (i = 0; i < string.length; i++) {
+      numberAscii = string.charCodeAt(i); // asigno una variable donde obtenga una caja que se
+      // almacene un texto y extraiga la posición ascii de cada letra que me da el arreglo(conjunto de espacios)
+      if (numberAscii >= 65 && numberAscii <= 90) {
+        newnumberAscii = (numberAscii + 65 - offset) % 26 + 65;
+        letterAscii = String.fromCharCode(newnumberAscii);
+      } else {
+        if (numberAscii >= 97 && numberAscii <= 122) {
+          newnumberAscii = (numberAscii - 122 - offset) % 26 + 122;
+          letterAscii = String.fromCharCode(newnumberAscii);
+        } else {
+          letterAscii = string.charAt(i);
+        }
+      }
+      textcod = textcod.concat(letterAscii);
+    }
+    return textcod;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 // Asignaré variables 
 // Se asignan variables para la implementación del código cesar
-let clickCifred=()=>
-{//El usuario ingresa su  mensaje aquí
-    let texttext=document.getElementById('message').value;
+
+let numberOffset; // Declaro variable para almancenar offset tipo número
+let clickCifred = () => {//El usuario ingresa su  mensaje aquí
+    let texttext = document.getElementById('message').value;
     //Varible para colocar el número que se desea desplazar a la derecha(cifrado) o izquierda
     //o (descrifrado)
-   let offsetreal=document.getElementById('offs').value
-    parseInt(offsetreal);
-    let numOffset= parseInt(offsetreal);
-    cipher.encode(texttext,offsetreal);
+    let offsetreal = document.getElementById('offset').value;
+    numberOffset= parseInt(offsetreal);
+    let texresult = cipher.encode(numberOffset, texttext);
+    document.getElementById('result').innerHTML = texresult;
 }
+document.getElementById('ci').addEventListener('click', clickCifred);
 
- document.getElementById('ci').addEventListener('click',clickCifred);
+let clickDecifred = () => {//El usuario ingresa su  mensaje aquí
+    let texttext = document.getElementById('message').value;
+    //Varible para colocar el número que se desea desplazar a la derecha(cifrado) o izquierda
+    //o (descrifrado)
+    let offsetreal = document.getElementById('offset').value;
+    numberOffset= parseInt(offsetreal);
+    let texresult = cipher.decode(numberOffset, texttext);
+    document.getElementById('result').innerHTML = texresult;   
+}
+document.getElementById('deci').addEventListener('click', clickDecifred);
