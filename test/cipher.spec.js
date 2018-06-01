d227f90cb1ca18ea373a6cf64e03af2f8f3501ab
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-06-01 18:49:16 -0500

    Intentando que sea responsive

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -4,22 +4,26 @@ describe('cipher', () => {
     assert.equal(typeof cipher, 'object');
   });
 
-  describe('cipher.encode', () => {
 
-    it('debería ser una función', () => {
+describe('cipher.encode', () => {
+
+  it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+  it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
       assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
-    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+  it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
       assert.equal(cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"), "hijklmnopqrstuvwxyzabcdefg");
     });
-    it('debería retornar "HIJ hij" para "ABC abc" con offset 33', () => {
+  it('debería retornar "HIJ hij" para "ABC abc" con offset 33', () => {
       assert.equal(cipher.encode(33, "ABC abc"), "HIJ hij");
     });
   });
+
+
+
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
@@ -37,6 +41,8 @@ describe('cipher', () => {
     });
   });
 
+
+
   describe('cipher.createCipherWithOffset', () => {
 
     it('debería ser una función', () => {
@@ -47,30 +53,27 @@ describe('cipher', () => {
       assert.equal(typeof cipher.createCipherWithOffset(33), 'object');
       assert.property(cipher.createCipherWithOffset(33), 'encode');
       assert.property(cipher.createCipherWithOffset(33), 'decode');
-
       it('debería retornar "HIJ hij" para "ABC abc" con offset 33', () => {
         assert.equal(cipher.createCipherWithOffset(33).encode("ABC abc"), "HIJ hij");
       });
 
 
-      describe('cipher.createCipherWithOffset().encode', () => {
-      it('debería ser una función',() => {
-        assert.equal(typeof cipher.createCipherWithOffset(33).encode, 'function');
-      });
-      
-      });
-
-
       describe('cipher.createCipherWithOffset().decode', () => {
-        it('debería ser una función',() => {
+        it('debería ser una función', () => {
           assert.equal(typeof cipher.createCipherWithOffset(33).decode, 'function');
         });
         it('debería retornar "ABC abc" para "HIJ hij" con offset 33', () => {
           assert.equal(cipher.createCipherWithOffset(33).decode("HIJ hij"), "ABC abc");
         });
-        });
+      });
 
+      describe('cipher.createCipherWithOffset().encode', () => {
+        it('debería ser una función', () => {
+          assert.equal(typeof cipher.createCipherWithOffset(33).encode, 'function');
+        });
+      });
     });
 
   });
+
 });
