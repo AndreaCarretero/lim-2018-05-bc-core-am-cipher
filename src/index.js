3b86b30fc580d6321a8c2e5299a487dd4ed2c174
LesAndreaCarretero <andrea.carretero2097@gmail.com>
2018-06-03 19:21:54 -0500

    Versión final al fin

--- src/index.js
+++ src/index.js
@@ -23,3 +23,25 @@ let clickDecifred = () => {//Asignando variable para click descrifrar
     document.getElementById('result').innerHTML = texresult;   
 }
 document.getElementById('deci').addEventListener('click', clickDecifred);
+
+
+
+let clickstep1 =() => {
+    document.getElementById('step1').style.visibility='visible';
+}
+document.getElementById('button1').addEventListener('click',clickstep1);
+
+let clickstep2 =() => {
+    document.getElementById('step2').style.visibility='visible';
+}
+document.getElementById('button2').addEventListener('click',clickstep2);
+
+let clickstep3 =() => {
+    document.getElementById('step3').style.visibility='visible';
+}
+document.getElementById('button3').addEventListener('click',clickstep3);
+
+let clickstep4 =() => {
+    document.getElementById('step4').style.visibility='visible';
+}
+document.getElementById('button4').addEventListener('click',clickstep4);
